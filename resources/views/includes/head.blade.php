1e822a39b98f9e20a1a3f20cf66f08032487ee9a
HiddeXA <hiddealberts@gmail.com>
2025-03-11 10:53:09 +0100

    feat: add background and layout

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gans & Chaos</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
